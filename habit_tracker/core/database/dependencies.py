461fb82d684dbbca72c8e98103b76f51fd467fdc
Mikhail Zotochev <zotochev@gmail.com>
2025-06-10 21:44:53 +0200

    moved database code to backend project. Removed it from bot

--- habit_tracker/core/database/dependencies.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from .db import AsyncSessionLocal
-
-
-async def get_db() -> AsyncSession:
-    async with AsyncSessionLocal() as session:
-        yield session
