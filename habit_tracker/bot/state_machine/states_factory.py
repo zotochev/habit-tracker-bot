3ec8e2d2d144e79e3d395b9b29fbc524d542a70d
Mikhail Zotochev <zotochev@gmail.com>
2025-06-25 22:10:45 +0200

    removed unused code

--- habit_tracker/bot/state_machine/states_factory.py
+++ habit_tracker/bot/state_machine/states_factory.py
@@ -1,28 +1,8 @@
 from bot.states import HabitStates
 
-# from bot.state_machine.states import (
-#     InitState,
-#     ChooseLanguage,
-#     ChooseLanguageCommand,
-#     WaitCommand,
-#     EndState,
-#     AddHabitCommandState,
-#     AddHabitState,
-# )
 
+STATES_FACTORY = {}
 
-STATES_FACTORY = {
-    # state_class.state: state_class
-    # for state_class in (
-    #     InitState,
-    #     ChooseLanguage,
-    #     ChooseLanguageCommand,
-    #     WaitCommand,
-    #     EndState,
-    #     AddHabitCommandState,
-    #     AddHabitState,
-    # )
-}
 
 def register_state(state: HabitStates):
     def _wrapper(cls):
