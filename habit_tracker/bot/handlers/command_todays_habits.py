f588d49871115b141cc26552235053d7b5d15e4a
Mikhail Zotochev <zotochev@gmail.com>
2025-09-18 20:50:37 +0200

    added: handle of todays_message command

--- habit_tracker/bot/handlers/command_todays_habits.py
+++ habit_tracker/bot/handlers/command_todays_habits.py
@@ -14,4 +14,6 @@ router = Router()
 async def todays_habit_handler(message: Message, user_cache: UserCache):
     if user_cache.state_machine.state not in (HabitStates.command_todays_habits, HabitStates.todays_habits):
         await user_cache.state_machine.set_state(HabitStates.todays_habits)
+
+    await user_cache.state_machine.handle(message)  # to update message
     await user_cache.messenger.remove_temp_messages()
